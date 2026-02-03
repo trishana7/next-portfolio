97c0037afc5868962c7b56b715abcc7fde1032a8
Trishana Rai <trishana@convergestack.com>
2026-02-02 23:07:01 -0600

    portfolio setup

---
+++ appData/personal.tsx
@@ -0,0 +1,7 @@
+import { GithubIcon, Instagram, LinkedIn} from '@/utils/icons'
+
+export const socials = [
+  { href: '', icon: <GithubIcon /> },
+  { href: '', icon: <LinkedIn /> },
+  { href: '', icon: <Instagram /> },
+]
